fbf451c840705050ae3504081c3ecb0ac8c182ec
unknown <papolopez@gmail.com>
2022-08-09 21:49:52 -0500

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "Hola soy papo";
+const nuevaString = "Hola soy pablo";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
